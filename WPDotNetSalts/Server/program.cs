8d8c6ccd8a128404950be599702e5c3d711c17bd
Daniel Llewellyn <daniel@bowlhat.net>
2020-08-18 18:34:30 +0100

    Bump peachpie to 0.9.990

--- WPDotNetSalts/Server/program.cs
+++ WPDotNetSalts/Server/program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Peachpie.AspNetCore.Web;
 
 namespace WPDotNetSalts.Server
@@ -21,14 +22,14 @@ namespace WPDotNetSalts.Server
 
     class Startup
     {
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UsePhp(new PhpRequestOptions(scriptAssemblyName: "WPDotNetSalts"));
+            app.UsePhp();
             app.UseDefaultFiles();
             app.UseStaticFiles();
         }
