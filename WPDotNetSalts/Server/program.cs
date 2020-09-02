19cdb93032f90448ee031fddc89c2d2b98974f57
Daniel Llewellyn <daniel@bowlhat.net>
2020-09-02 15:46:38 +0100

    Update to PP1.0.0-preview1

--- WPDotNetSalts/Server/program.cs
+++ WPDotNetSalts/Server/program.cs
@@ -1,10 +1,7 @@
-using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Peachpie.AspNetCore.Web;
 
 namespace WPDotNetSalts.Server
 {
