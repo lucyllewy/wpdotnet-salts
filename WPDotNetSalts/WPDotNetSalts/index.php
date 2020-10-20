962511c521d47df38b1770654048ea9f2e8bd939
Daniel Llewellyn <daniel@bowlhat.net>
2020-10-20 18:48:01 +0100

    Update to PP 1.0.0-preview3

--- WPDotNetSalts/WPDotNetSalts/index.php
+++ WPDotNetSalts/WPDotNetSalts/index.php
@@ -26,46 +26,76 @@ function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzAB
     return implode('', $pieces);
 }
 
-$keys = [
-    'AUTH_KEY',
-    'AUTH_SALT',
-    'LOGGED_IN_KEY',
-    'LOGGED_IN_SALT',
-    'NONCE_KEY',
-    'NONCE_SALT',
-    'SECURE_AUTH_KEY',
-    'SECURE_AUTH_SALT',
-];
-$values = [];
-$maxlength = array_reduce($keys, function($carry, $key, $idx) {
-    $len = strlen($key);
-    if ($len > $carry) {
-        return $len;
-    }
-    return $carry;
-}, 0);
+function generate_salts() {
+    $keys = [
+        'AUTH_KEY',
+        'AUTH_SALT',
+        'LOGGED_IN_KEY',
+        'LOGGED_IN_SALT',
+        'NONCE_KEY',
+        'NONCE_SALT',
+        'SECURE_AUTH_KEY',
+        'SECURE_AUTH_SALT',
+    ];
+    $values = [];
+    $maxlength = array_reduce($keys, function($carry, $key) {
+        $len = strlen($key);
+        if ($len > $carry) {
+            return $len;
+        }
+        return $carry;
+    }, 0);
 
-foreach ($keys as $key) {
-	$values[$key] = random_str( 64 );
+    foreach ($keys as $key) {
+	    $values[$key] = random_str( 64 );
+    }
+
+    echo '"SALT": {' . "\n";
+    while ($key = array_shift($keys)) {
+        echo '	"' . $key . '":';
+        for ($i = strlen($key); $i <= $maxlength; $i++) {
+            echo ' ';
+        }
+        echo '"' . $values[$key] . '"';
+        if (0 < count($keys)) {
+            echo ',';
+        }
+        echo "\n";
+    }
+    echo '}';
 }
+
+if (isset($_GET['raw'])) {
+    header('Content-Type: text/plain', true, 200);
+    generate_salts();
+} else {
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <style>
     body {
-        margin: 0 auto;
-        max-width: 800px;
+        display: grid;
+        height: 100vh;
+        margin: 0;
+    }
+    body > div {
+        align-self: center;
+        justify-self: center;
+        width: min-content;
     }
     h1 {
+        margin: 0 0 3rem;
         text-align: center;
     }
     pre {
         background-color: #f0f0f0;
         border: 0.1rem dashed #cccccc;
+        max-width: 80vw;
+        overflow-x: auto;
         padding: 1rem;
     }
-    pre, h1 {
+    pre {
         margin: 3rem 0;
     }
     </style>
@@ -77,15 +107,13 @@ foreach ($keys as $key) {
 <p>Your salts and keys for WPdotNet appsettings.json are below:</p>
 
 <pre>
-"SALT": {
-<?php while ($key = array_shift($keys)) : ?>
-	"<?php echo $key; ?>":<?php for ($i = strlen($key); $i <= $maxlength; $i++) echo " "; ?>"<?php echo $values[$key]; ?>"<?php if (0 < count($keys)) { echo ','; } ?>
-
-<?php endwhile; ?>
-}
+<?php generate_salts(); ?>
 </pre>
 
-<p>These salts are for use with <a href="https://www.wpdotnet.com/">WPdotNet</a> from <a href="https://www.peachpie.io/">PeachPie PHP Compiler for .NET</a> and <a href="http://www.iolevel.com/">IOLevel</a>. Make your own WordPress site running on .NET using the <a href="https://github.com/iolevel/peachpie-wordpress/">WPdotNet boilerplate at GitHub</a>, which is based on the NuGet packages created from the <a href="https://github.com/iolevel/wpdotnet-sdk">WPdotNet SDK on github</a>.
+<p>These salts are for use with <a href="https://www.wpdotnet.com/">WPdotNet</a> from <a href="https://www.peachpie.io/">PeachPie PHP Compiler for .NET</a> and <a href="http://www.iolevel.com/">IOLevel</a>. Make your own WordPress site running on .NET using the <a href="https://github.com/iolevel/peachpie-wordpress/">WPdotNet boilerplate at GitHub</a>, which is based on the NuGet packages created from the <a href="https://github.com/iolevel/wpdotnet-sdk">WPdotNet SDK on github</a>.</p>
+<p>The source code for this generator is publically accessible at <a href="https://github.com/diddledan/wpdotnet-salts">GitHub</a>.</p>
 </div>
 </body>
 </html>
+<?php
+}
